d583475906501b700e1462d7e2db04a369d99e67
Aleksandr <56115945+AlSavIg@users.noreply.github.com+alesavigo@gmail.com>
2022-10-04 20:26:26 +0300

    config

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
-
 cookies = {
-    '__ddg1_': 'gPjm9poIKDAZ8S0GjD88',
-    '_ga': 'GA1.2.198270785.1664895047',
-    '_gid': 'GA1.2.1799822891.1664895047',
+    '__ddg1_': 'MpmoAerxXM9MYlFKjS2e',
+    '_ga': 'GA1.2.15648870.1664818325',
+    '_gid': 'GA1.2.1381386884.1664818325',
+    '_ym_uid': '1664819270528110360',
+    '_ym_d': '1664819270',
+    '_gat': '1',
+    '_ym_isad': '1',
 }
 
 headers = {
     'authority': 'kompromat1.pro',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'ru-RU,ru;q=0.9',
-    'cache-control': 'no-cache',
-    # Requests sorts cookies= alphabetically
-    # 'cookie': '__ddg1_=gPjm9poIKDAZ8S0GjD88; _ga=GA1.2.198270785.1664895047; _gid=GA1.2.1799822891.1664895047',
-    'pragma': 'no-cache',
-    'referer': 'https://kompromat1.pro/articles/dictators',
-    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+              'application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'ru,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'referer': 'https://kompromat1.pro/articles/archive/2014-05-07',
+    'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Yandex";v="22"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
     'sec-fetch-dest': 'document',
@@ -22,7 +23,8 @@ headers = {
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/104.0.5112.124 YaBrowser/22.9.2.1495 Yowser/2.5 Safari/537.36',
 }
 
-url = 'https://kompromat1.pro/articles'
+url = 'https://kompromat1.pro/articles/668-kurchenko_kupiv_naftoperevalku_zlochevsjkogo'
--- parser.py
+++ parser.py
@@ -2,20 +2,23 @@ import time
 import aiohttp
 import asyncio
 import cfscrape
+import requests
 from parse_package.multypurpose_parser import ScrapSession
 from config import cookies, headers, url
 from bs4 import BeautifulSoup
 
 
 async def get_page(num_of_page):
-    async with (aiohttp.ClientSession()) as session:
+    async with aiohttp.ClientSession() as session:
         session = cfscrape.create_scraper(session)
-        async with session.get('https://kompromat1.pro/articles?pg=' + str(num_of_page)
-                , cookies=cookies, headers=headers) as response:
-            data = []
-            data.append(response)
-            await asyncio.sleep(1)
-            return data
+        # session = cfscrape.create_scraper(sess=requests.Session())
+        response = session.get(url=url,
+                               cookies=cookies,
+                               headers=headers)
+        data = []
+        data.append(response)
+        await asyncio.sleep(1)
+        return data
     # data = []
     # session = ScrapSession()
     # response = session.get('https://kompromat1.pro/articles?pg=' + str(num_of_page), proxies=True, cookies=cookies
