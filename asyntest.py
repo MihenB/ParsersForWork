2c4f5482541de0e87c9f98719c9c968729fb291e
alexander <alesavigo@gmail.com>
2022-10-05 22:19:31 +0300

    Updated README.md for all projects

--- asyntest.py
+++ asyntest.py
@@ -1,53 +1,61 @@
-from parse_package.multypurpose_parser import ScrapSession
+# from parse_package.multypurpose_parser import ScrapSession
 from config import cookies, headers
-link_root = 'https://kompromat1.pro'
 import asyncio
 import cfscrape
 import aiohttp
 from bs4 import BeautifulSoup
 import time
+from aiocfscrape import CloudflareScraper
+
+link_root = 'https://kompromat1.pro'
+
 
 def write_to_txt(data):
     with open('links.txt', 'w') as file:
         for link in data:
-            file.writelines(link +'\n')
+            file.writelines(link + '\n')
 
 
-async def get(num):
-    data = []
-    async with aiohttp.ClientSession() as session:
-        params = {
-            'pg': str(num),
-        }
-        session = cfscrape.create_scraper(session)
-        resp = session.get('https://kompromat1.pro/articles', params=params, cookies=cookies, headers=headers)
-        resp = BeautifulSoup(resp.text, 'lxml')
-        cards = resp.find_all('a', class_='articles_title')
-        for card in cards:
-            link = card.get('href')
-            data.append(link_root + link)
-        #await asyncio.sleep(0)
-    return data
+async def get(num, session: CloudflareScraper, file):
+    # session = cfscrape.create_scraper(sess=session)
+    params = {
+        'pg': str(num),
+    }
+    # session = cfscrape.create_scraper(session)
+    # resp = await session.get('https://kompromat1.pro/articles', params=params, cookies=cookies, headers=headers)
+    resp = await session.get('https://google.com', headers=headers)
+    resp = BeautifulSoup(await resp.text(), 'lxml')
+    cards = resp.find_all('a', class_='articles_title')
+    print(num)
+    for card in cards:
+        link = card.get('href')
+        file.write(f'{link_root}{link}\n')
+    # await asyncio.sleep(0)
 
 
-def get_links():
-    data = []
-    for num_of_page in range(1000, 14193):
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-        data += asyncio.run(get(num_of_page))
-        #print(len(data))
-        print(num_of_page + 1)
-    return data
+async def get_links():
+    # data = []
+    async with CloudflareScraper() as session:
+        with open('links.txt', 'a', encoding='utf-8') as file:
+            tasks = [asyncio.create_task(get(num_of_page, session, file))
+                     for num_of_page in range(1000, 14193)]
+    # for num_of_page in range(1000, 14193):
+    #     # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    #     data += asyncio.run(get(num_of_page))
+    #     # print(len(data))
+    #     print(num_of_page + 1)
+    # return data
+            await asyncio.gather(*tasks)
 
 
-def main():
+async def main():
     str_time = time.time()
-    data = get_links()
-    print(len(data))
-    write_to_txt(data)
+    await get_links()
+    # print(len(data))
+    # write_to_txt(data)
     end_time = time.time() - str_time
-    print(end_time)
+    print(f'Execution time: {end_time}')
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
