c5a8d0638cead518b4c71e9bf934fb5a19140eb3
nikita <bingin62@gmail.com>
2022-10-07 17:41:22 +0000

    Добавлен прототип сборщика данных с сайта

--- data_former.py
+++ data_former.py
@@ -1,42 +1,27 @@
 from parse_package.multypurpose_parser import ScrapSession
 from config import cookies, headers
-link_root = 'https://kompromat1.pro'
-import asyncio
-import cfscrape
-import aiohttp
-from bs4 import BeautifulSoup
-import time
 
 
-# async def get(url):
-#     data = []
-#     async with aiohttp.ClientSession() as session:
-#         session = cfscrape.create_scraper(session)
-#         resp = session.get(url, cookies=cookies, headers=headers)
-#         resp = BeautifulSoup(resp.text, 'lxml')
-#         article = resp.find_all('a', class_='articles_title')
-#
-#     return data
-#
-#
-# def get_links():
-#     data = []
-#     for num_of_page in range(1000, 14193):
-#         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-#         data += asyncio.run(get(num_of_page))
-#         #print(len(data))
-#         print(num_of_page + 1)
-#     return data
-#
-#
-# def main():
-#     str_time = time.time()
-#     data = get_links()
-#     print(len(data))
-#     write_to_txt(data)
-#     end_time = time.time() - str_time
-#     print(end_time)
-#
-#
-# if __name__ == '__main__':
-#     main()
+def get_info_from_site(url, session):
+    soup = session.get(url=url, cookies=cookies, headers=headers, secured=True).soup
+    title = soup.find('h1').text
+    photo_link = soup.find('img').get('src')
+    text = soup.find('div', class_='articles_one k1_exy2xy1axay').text.strip()
+    # tags =
+    data = {
+        'title': title,
+        'photo_link': photo_link,
+        'text': text
+    }
+    return data
+
+
+def main():
+    session = ScrapSession()
+    link = 'https://kompromat1.pro/articles/216592-repera_pashu_tehnika_gospitalizirovali_v_botkinskuju_boljnitsu'
+    data = get_info_from_site(link, session)
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
